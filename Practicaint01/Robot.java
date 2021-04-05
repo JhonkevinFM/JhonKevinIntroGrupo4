1506b23e3305dde53385e9c8c939cc40a37acf5c
JhonkevinFM <44672518+JhonkevinFM@users.noreply.github.com>
2021-04-05 07:13:58 -0700

    Add files via upload

---
+++ Practicaint01/Robot.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Robot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Robot
+{
+   public static void main (String args[]){
+     System.out.println("Hola mundo"); 
+   }
+}
